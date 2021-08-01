cee5d89565e838b04eefe0db1eb307ea91a52ec8
Parth Parpyani <parthparpyani@Parths-MacBook-Pro.local>
2021-08-01 16:08:01 -0400

    port change

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const app = require('./app')
 const config = require('./utils/config')
+const key = fs.readFileSync('./server.key')
 const http = require('http')
 const server = http.createServer(app)
 
-server.listen(config.PORT, () => {
+server.listen(config.PORT || 3001, () => {
     console.log(`Server running on port ${config.PORT}`)
 })  
